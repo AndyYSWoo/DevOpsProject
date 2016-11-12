cd4864018ced2b38e4731a6d7e07d0c3317e6404
131250106 <1575234006@qq.com>
2016-11-12 21:46:29 +0800

    add try catch in Contexthelper

--- src/main/java/cn/edu/nju/se/DevOpsProject/util/ContextHelper.java
+++ src/main/java/cn/edu/nju/se/DevOpsProject/util/ContextHelper.java
@@ -4,7 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ContextHelper {
-	private static ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+	private static ApplicationContext context = null;
+	static{
+		try{
+			context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		}catch(Exception e){
+			System.out.println("=================");
+			e.printStackTrace();
+		}
+	}
 	public static Object getBean(String id){
 		return context.getBean(id);
 	}
