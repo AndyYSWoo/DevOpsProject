8964415513e19c48c63e84e733cf70654bca6c8d
methylene_blue <a3221666@sina.cn>
2016-11-13 11:58:29 +0800

    threshold

--- src/main/java/cn/edu/nju/se/DevOpsProject/dao/ThresholdDao.java
+++ src/main/java/cn/edu/nju/se/DevOpsProject/dao/ThresholdDao.java
@@ -1,20 +1,33 @@
 package cn.edu.nju.se.DevOpsProject.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import cn.edu.nju.se.DevOpsProject.model.Risk;
 import cn.edu.nju.se.DevOpsProject.model.RiskEntry;
 public class ThresholdDao {
 	private SessionFactory sessionFactory;
-	public void createEntry(RiskEntry entry){
+	public int getThresholdId(String threshold){
 		Session session = sessionFactory.openSession();
 		
-		//Transaction tx = session.beginTransaction();
-		//session.save(entry);
-		//tx.commit();
-		session.close();	
+		//ArrayList<Risk> list = new ArrayList<Risk>();
+		//Session session = sessionFactory.openSession();
+		String sql = "FROM Threshold WHERE content = "+"'"+threshold+"'";
+		Query query = session.createQuery(sql);
+		List<?> resultList = query.list();
+		if(resultList.size() == 0){
+			
+		}
+		session.close();
+		return 1;
+			
 	}
+	
 	public SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
